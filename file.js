e71053ff2c3a114a5ad1ad10293fbfb8e7c7fcac
eliab23 <gezahagnlema4@gmail.com>
2026-02-24 02:35:07 -0800

    Refactor file reading logic and uncomment relevant code

--- file.js
+++ file.js
@@ -1,26 +1,17 @@
 const fs = require("node:fs")
 //Read a file 
 
-// const tex = fs.readFile("./text.text", "utf-8",  (error, data) =>{
-//     if(error){
-//         console.log(error);
+const tex = fs.readFile("./text.text", "utf-8",  (error, data) =>{
+    if(error){
+        console.log(error);
     
-//     }else {
-//         console.log(data);
-//     }
-// })
-// console.log(tex);
+    }else {
+        console.log(data);
+    }
+})
+console.log(tex);
 
-// creating new file
-
-// fs.writeFile("./text1.txt", "Subscribe",  (error) =>{
-//     if(error){
-//         console.log(error);
-    
-//     }else {
-//         console.log("file created");
-//     }
-// })
+//creating new file
 
 // updating a file
 // fs.appendFile()
